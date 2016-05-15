51598a5013c54f2d18eef879d506821d19e19fbe
Broch Stilley <broch@lumi.com>
2016-05-15 16:45:32 -0700

    Add simple box to app page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Akkad, Scene, Material, cameras, lights, shapes, systems} from 'akkad';
 
-class App extends React.Component {
+const {ArcRotateCamera} = cameras;
+const {HemisphericLight} = lights;
+const {Box} = shapes;
+const {Color} = systems;
+
+class App extends Component {
     render() {
         return (
             <div className="app">
-                Akkad boilerplate
+                <h1>Akkad boilerplate</h1>
+                <Akkad>
+                    <Scene>
+                        <ArcRotateCamera
+                            position={[3, 4, -5]}
+                            target={[0, 1, 0]}
+                        />
+                        <HemisphericLight />
+                        <Box>
+                            <Material>
+                                <Color color={[0.2, 0.5, 0.7]} />
+                            </Material>
+                        </Box>
+                    </Scene>
+                </Akkad>
             </div>
         )
     }
